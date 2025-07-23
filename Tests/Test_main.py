51d648898bba574ea810f04eff47871f64a24601
NaveenDas1 <111351691+NaveenDas1@users.noreply.github.com>
2025-07-23 12:58:54 +0530

    Python Unittest

--- Tests/Test_main.py
+++ Tests/Test_main.py
@@ -1,7 +1,11 @@
-from Source_code.main import add
+import unittest
+from add import add
 
-def test_fun():
-    assert add(2,3) == 5
-    assert add(28,22) == 50
-    assert add(97,2) == 99    
+class TestAdd(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
 
+if __name__ == "__main__":
+    unittest.main()
