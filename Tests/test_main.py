aa196baba3ac7ab2e488e4cc032dc58d2c0316a1
NaveenDas1 <111351691+NaveenDas1@users.noreply.github.com>
2025-07-24 12:17:45 +0530

    updated test_main.py file

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -1,11 +1,13 @@
 import unittest
-from add import add
-
+from Souce_code.main import add
 class TestAdd(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(2,3),5)
 
 if __name__ == "__main__":
     unittest.main()
+
+
